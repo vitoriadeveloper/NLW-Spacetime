9d28a81ecf927b71627f9f944273d98f0422eac3
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2023-05-18 11:34:16 -0300

    fixed mobile bug

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
+  content: ['./App.tsx', './app/**/*.tsx'],
   theme: {
     extend: {
-      fontFamily:{
+      fontFamily: {
         title: 'Roboto_700Bold',
-        body:'Roboto_400Regular',
-        alt: 'BaiJamjuree_700Bold'
+        body: 'Roboto_400Regular',
+        alt: 'BaiJamjuree_700Bold',
       },
+
       colors: {
         gray: {
           50: '#eaeaea',
@@ -33,30 +34,6 @@ module.exports = {
           800: '#48307e',
           900: '#372560',
         },
-        orange: {
-          50: '#ffefeb',
-          100: '#ffccc2',
-          200: '#ffb4a4',
-          300: '#ff927b',
-          400: '#ff7d61',
-          500: '#ff5c3a',
-          600: '#e85435',
-          700: '#b54129',
-          800: '#8c3320',
-          900: '#6b2718',
-        },
-        yellow: {
-          50: '#fff9ec',
-          100: '#ffebc4',
-          200: '#ffe2a7',
-          300: '#ffd47f',
-          400: '#ffcc66',
-          500: '#ffbf40',
-          600: '#e8ae3a',
-          700: '#b5882d',
-          800: '#8c6923',
-          900: '#6b501b',
-        },
         green: {
           50: '#e6fbef',
           100: '#b1f1ce',
@@ -74,4 +51,3 @@ module.exports = {
   },
   plugins: [],
 }
-
