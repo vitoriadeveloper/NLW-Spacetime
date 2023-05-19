f36ebd61603a5db43c7fad8ed7df456db696192a
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2023-05-19 11:14:55 -0300

    memories route mobile implemented

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,53 @@
+import { styled } from 'nativewind'
+import React, { useEffect, useState } from 'react'
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import { ImageBackground } from 'react-native'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { useFonts } from 'expo-font'
+import { SplashScreen, Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import * as SecureStore from 'expo-secure-store'
+
+export default function Layout() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<
+    null | boolean
+  >(null)
+  const StyledStripes = styled(Stripes)
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) => {
+      setIsUserAuthenticated(!!token) // the two exclamation marks convert the token to a bolean, cause token comes as string
+    })
+  }, [])
+  if (!hasLoadedFonts) {
+    return <SplashScreen />
+  }
+  return (
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 bg-gray-900"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+      <StatusBar style="light" translucent />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: 'transparent' },
+        }}
+      >
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        {/* if user authenticated, redirect to memories */}
+        <Stack.Screen name="new" />
+        <Stack.Screen name="memories" />
+      </Stack>
+    </ImageBackground>
+  )
+}
