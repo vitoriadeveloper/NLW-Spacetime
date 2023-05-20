3390609126a0409d151a1a2d0f7faab7f002b1d5
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2023-05-20 10:41:29 -0300

    front finished

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,62 @@
 import { EmptyMemories } from '@/components/EmptyMemories'
+import { api } from '@/lib/api'
+import dayjs from 'dayjs'
+import { ArrowRight } from 'lucide-react'
+import { cookies } from 'next/headers'
+import Image from 'next/image'
+import Link from 'next/link'
+interface Memory {
+  id: string
+  coverUrl: string
+  createdAt: string
+  excerpt: string
+}
+export default async function Home() {
+  const isAuthenticate = cookies().has('token')
+  if (!isAuthenticate) {
+    return <EmptyMemories />
+  }
 
-export default function Home() {
-  return <EmptyMemories />
+  const token = cookies().get('token')?.value
+  const response = await api.get('/memories', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+  const memories: Memory[] = response.data
+
+  if (memories.length === 0) {
+    return <EmptyMemories />
+  }
+
+  return (
+    <div className="flex flex-col gap-10 p-8">
+      {memories.map((memory) => {
+        return (
+          <div key={memory.id} className="space-y-4">
+            <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
+              {dayjs(memory.createdAt).format('D[ de ]MMMM[, ]YYYY')}
+            </time>
+            <Image
+              src={memory.coverUrl}
+              alt=""
+              width={592}
+              height={280}
+              className="aspect-video w-full rounded-lg object-cover"
+            />
+            <p className="text-lg leading-relaxed text-gray-100">
+              {memory.excerpt}
+            </p>
+            <Link
+              href={`/memories/${memory.id}`}
+              className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-100"
+            >
+              Ler mais
+              <ArrowRight className="h-4 w-4" />
+            </Link>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
